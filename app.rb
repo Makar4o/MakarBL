98d40ddab4368b4e68cdbfef3e8f25d669a9ef82
Makar4o <makarmakar4o@gmail.com>
2016-12-06 04:23:42 +0300

    Read text from variable content

--- app.rb
+++ app.rb
@@ -5,10 +5,16 @@ require 'sinatra'
 
 get '/' do
 
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+  erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
 end
 
 get '/new' do
 
-	erb :new
+  erb :new
+end
+
+post '/new' do
+  content = params[:content]
+
+  erb "You typed #{content}"
 end
